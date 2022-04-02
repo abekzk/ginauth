9d01dd6cd6ea1ed3ad94a4d43eb9736c2fa7e86e
kzuabe <ai3ekzk@gmail.com>
2022-04-02 12:33:50 +0900

    feat: add NewFirebaseClient func

--- firebase.go
+++ firebase.go
@@ -1,5 +1,23 @@
 package ginauth
 
-import "firebase.google.com/go/auth"
+import (
+	"context"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+)
 
 type FirebaseClient *auth.Client
+
+// ref: https://github.com/firebase/firebase-admin-go/blob/e60757f9b29711f19fa1f44ce9b5a6fae3baf3a5/snippets/init.go#L82-L98
+func NewFirebaseClient() (FirebaseClient, error) {
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
